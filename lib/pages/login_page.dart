e519440301be14fa860d67cb1dd92f7f8598a6c2
persilee <i@lishaoy.net>
2019-07-16 00:19:05 +0800

    🍊 add loading for login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pay_cost/navigator/tab_navigater.dart';
+import 'package:pay_cost/util/toast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -7,11 +10,79 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _loginKey = GlobalKey<FormState>();
+  String uName, uPwd;
+  bool autoValidate = false;
+  bool _loading = false;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
+  void submitRegisterForm() {
+    if (_loginKey.currentState.validate()) {
+      _loginKey.currentState.save();
+      _onRefresh();
+    } else {
+      setState(() {
+        autoValidate = true;
+      });
+    }
+  }
+
+  String validatorName(value) {
+    if (value.isEmpty) {
+      return '用户名不能为空';
+    }
+
+    return null;
+  }
+
+  String validatorPwd(value) {
+    if (value.isEmpty) {
+      return '密码不能为空';
+    }
+
+    return null;
+  }
+
+  Future<Null> _onRefresh() async {
+    setState(() {
+      _loading = !_loading;
+      autoValidate = true;
+    });
+    await Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        _loading = !_loading;
+        autoValidate = true;
+      });
+      if(uName == 'lsy' && uPwd == 'lsy123') {
+        save();
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => TabNavigator()));
+      }else{
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
+        Toast.show(context, "用户名或密码不正确");
+      }
+    });
+  }
+
+  save() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('name', uName);
+    prefs.setString('pwd', uPwd);
+  }
+
+  Widget _childLayout() {
+    if (_loading) {
+      return Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(),
+              Padding(padding: EdgeInsets.only(top: 16),),
+              Text('登录中...'),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return ListView(
         children: <Widget>[
           Container(
             height: 600,
@@ -32,14 +103,47 @@ class _LoginPageState extends State<LoginPage> {
                       TextFormField(
                         decoration: InputDecoration(
                           labelText: '用户名',
-                          icon: Icon(Icons.person,),
+                          icon: Icon(
+                            Icons.person,
+                          ),
                         ),
+                        onSaved: (value) {
+                          uName = value;
+                        },
+                        validator: validatorName,
+                        autovalidate: autoValidate,
                       ),
                       Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 10)),
                       TextFormField(
+                        obscureText: true,
                         decoration: InputDecoration(
                           labelText: '密码',
-                          icon: Icon(Icons.lock,),
+                          icon: Icon(
+                            Icons.lock,
+                          ),
+                        ),
+                        onSaved: (value) {
+                          uPwd = value;
+                        },
+                        validator: validatorPwd,
+                        autovalidate: autoValidate,
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        child: RaisedButton(
+                          color: Theme.of(context).primaryColor,
+                          child: Text(
+                            '登 录',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          elevation: 0.0,
+                          onPressed: submitRegisterForm,
+                          shape: StadiumBorder(),
                         ),
                       ),
                     ],
@@ -49,7 +153,13 @@ class _LoginPageState extends State<LoginPage> {
             ),
           ),
         ],
-      ),
+      );
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _childLayout(),
     );
   }
 }
